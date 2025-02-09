3991eb377b42ba6a7d44a6ec805d6a920c31da9a
Milene Ruth <milene.ruth.ruth@outlook.com>
2025-02-09 12:37:59 -0300

    correção

--- python test/desafio 18.py
+++ python test/desafio 18.py
@@ -1,6 +1,6 @@
-import math
-ang = float(input('Digite um angulo que você deseja: '))
-s = math.sin(math.radians(ang))
-c = math.cos(math.radians(ang))
-t = math.tan(math.radians(ang))
-print('O ângulo de {} \nTem o seno: {:.2f} \nO cosseno: {:.2f} \nE sua tamgemte: {:.2f} '.format(ang, s, c, t))
+from math import radians,sin,cos,tan
+ang = float(input("Digite o ângulo: "))
+s = sin(radians(ang))
+c = cos(radians(ang))
+t = tan(radians(ang))
+print("o ângulo: {:.2f} \nCorresponde ao seno: {:.2f} \nÉ o cosseno: {:.2f} \nÉ sua tangente: {:.2f}".format(ang, s, c, t))
