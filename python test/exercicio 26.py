242757c06065bdeca69c2ce70591775054ee2d2e
Milene Ruth <milene.ruth.ruth@outlook.com>
2025-02-23 22:22:23 -0300

    exercício 26

---
+++ python test/exercicio 26.py
@@ -0,0 +1,4 @@
+frase = input('Digite uma frase: ')
+print('A letra (A) aparece {}'.format(frase.count('A')))
+print('Sua posição primeira e {}'.format(frase.find('A')))
+print('Sua ultima posição {}: '.format(frase.rfind('A')))
