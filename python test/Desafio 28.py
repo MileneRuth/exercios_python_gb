74e42767ed5ce0c61fb82cb2437d010acb5f4962
Milene Ruth <milene.ruth.ruth@outlook.com>
2025-03-05 11:22:48 -0300

    Desafio 28
    tentei ksksk tentei usar o while e o for, mas não soube, mesmo depois de ler horrores

---
+++ python test/Desafio 28.py
@@ -0,0 +1,6 @@
+print('Tente adivinhar entre 0 e 5, qual número foi escolhido ')
+num = int(input('Digite um número: '))  
+if num == 5:  
+    print('acertou miseravel')  
+else:  
+    print('não foi dessa vez. ')
