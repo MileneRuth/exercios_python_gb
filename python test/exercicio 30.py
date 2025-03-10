e86804346130705eb287be5fb2b92ea6d6519e06
Milene Ruth <milene.ruth.ruth@outlook.com>
2025-03-09 23:40:16 -0300

    exercício 30
    ain esse eu adoroooo, já tinha feito na facul, mas e sempre bom relembrar

---
+++ python test/exercicio 30.py
@@ -0,0 +1,8 @@
+num = int(input('Me diga um número para saber se e impar ou par ou 0 para sair : '))
+while num != 0:
+    if num % 2 == 0:
+        print('Numero par')
+    else:
+        print('número impar')
+    num = int(input('Me diga um número para saber se e impar ou par ou 0 para sair '))
+print('Adios até breve')
