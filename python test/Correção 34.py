46ba5c80166b5355ce0c0bef7bf35f878f4a8dd5
Milene Ruth <milene.ruth.ruth@outlook.com>
2025-03-17 22:32:52 -0300

    correção 34
    achei mais fácil assim

--- python test/Correção 34.py
+++ python test/Correção 34.py
@@ -3,4 +3,4 @@ if salario <= 1250:
     novo = salario + (salario * 15 / 100)
 else:
     novo = salario + (salario * 10 / 100)
-print('O salario {} com o reajuse vai para {}'.format(salario,novo))
+print('O salario {:.2f} com o reajuse vai para {:.2f}'.format(salario,novo))
