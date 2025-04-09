9f4b8de0e117904a8f2bc1b92dd9e207eb0229d5
Milene Ruth <milene.ruth.ruth@outlook.com>
2025-04-08 21:54:36 -0300

    Correção do exercicio 36

--- modulo 2/Desafio036.py
+++ modulo 2/Desafio036.py
@@ -1,5 +1,12 @@
 print('\033[1;32mEmprestimo Consignado\033[m')
-valor = float(input('Digite o valor da casa R$: '))
-sl = float(input('Quanto você receb por mês R$: '))
-anos = valor / sl
-print('Você paraga R${:.2f} por tantos anos'.format(anos))
+casa = float(input('Digite o valor da casa R$: '))
+sl = float(input('Quanto você recebe por mês R$: '))
+anos = int(input('Quantos anos de financiamento? '))
+prestação = casa / (anos * 12)
+minimo = sl * 30 / 100
+print('Você para pagar uma casa de R${:.2f} em {} anos. '.format(casa, anos), end='')
+print('a Prestação será de R${:.2f}'.format(prestação))
+if prestação <= minimo:
+    print('Emprestimo pode ser \033[1;32mConcedido\033[m')
+else:
+    print('Emprestimo \033[1;31mNegado\033[m')
