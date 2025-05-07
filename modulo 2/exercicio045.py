4902094c3652cdbd18fcf0271b37fff9a1a3ce9f
Milene Ruth <milene.ruth.ruth@outlook.com>
2025-05-06 22:38:24 -0300

    exercicio e correção 45

---
+++ modulo 2/exercicio045.py
@@ -0,0 +1,39 @@
+from random import randint
+itens = ('Pedra','Papel', 'Tesoura')
+pc = randint(0, 2)
+print('''Suas opções são:
+ [ 0 ] PEDRA
+ [ 1 ] PAPEL
+ [ 2 ] TESOURA''')
+jogador = int(input('Qual e sua jogada? '))
+print('\033[1:31m-=\033[m' * 30)
+print('O computador escolheu \033[1:33m{}\033[m'.format(itens[pc]))
+print('Jogador jogou \033[1:32m{}\033[m'.format(itens[jogador]))
+print('\033[1:31m-=\033[m' * 30)
+if pc == 0:
+    if jogador == 0:
+        print('EMPATE')
+    elif jogador == 1:
+        print('JOGADOR VENCE')
+    elif jogador == 2:
+        print('COMPUTADOR VENCE')
+    else:
+        print('Jogada invalida')
+elif pc == 1:
+    if jogador == 0:
+        print('COMPUTADOR VENCE')
+    elif jogador == 1:
+        print('EMPATE')
+    elif jogador == 2:
+        print('JOGADOR VENCE')
+    else:
+        print('Jogada invalida')
+elif pc == 2:
+    if jogador == 0:
+        print('JOGADOR VENCE')
+    elif jogador == 1:
+        print('COMPUTADOR VENCE')
+    elif jogador == 2:
+        print('EMPATE')
+    else:
+        print('Jogada invalida')
