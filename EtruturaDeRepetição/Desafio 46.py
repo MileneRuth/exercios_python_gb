1078aa8088fca8bf2439664a96d60c1259694225
Milene Ruth <milene.ruth.ruth@outlook.com>
2025-05-12 16:19:36 -0300

    Desafio 46
    acho que fiz certo

---
+++ EtruturaDeRepetição/Desafio 46.py
@@ -0,0 +1,8 @@
+from time import sleep
+
+import emoji
+import imoji
+print('Contagem regressiva')
+for c in range(0, 10):
+    print(c, sleep(1))
+print(emoji.emojize('BUM :fireworks:', language = 'alias'))
