ff2d0d15f16b661dfed05782835c9c908e19fcf0
Milene Ruth <milene.ruth.ruth@outlook.com>
2025-05-14 19:01:30 -0300

    Desafios 47, 48 e 49

---
+++ EtruturaDeRepetição/Desafio 47.py
@@ -0,0 +1,4 @@
+for num in range(1,51):
+    if num % 2 == 0:
+        print('O números pares encontrado entre 1 á 50 e : {}'.format(num))
+print('acabou')
---
+++ EtruturaDeRepetição/Desafio 49.py
@@ -0,0 +1,4 @@
+tabu = int(input('Digite uma número: '))
+for tabu in range(1,11):
+    print('print {} X {} = {}'.format(tabu,1, tabu*1))
+
