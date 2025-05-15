a76995a41d6f78fcc4ac270742dea55007f3de0d
Milene Ruth <milene.ruth.ruth@outlook.com>
2025-05-14 22:25:02 -0300

    Correção 48

--- EtruturaDeRepetição/Desafio 48.py
+++ EtruturaDeRepetição/Desafio 48.py
@@ -1,4 +1,7 @@
-for num in range(1,500):
-    if num % 3 ==0:
-        print(num)
-        continue
+soma = 0
+cont = 0
+for c in range(1, 501, 2):
+    if c % 3 == 0:
+        soma += c
+        cont += 1
+print('A soma de todos os {} valores solicitados é {}'.format(cont,soma))
