0e0905c76d01007f60d1707ca04b1b3e75a8353b
Milene Ruth <milene.ruth.ruth@outlook.com>
2025-05-26 22:29:40 -0300

    Desafio 52
    Eu utilixei o break, mas acho que a forma será utilizando.
    Divisores = 0 e tals

--- EtruturaDeRepetição/Desafio  52.py
+++ EtruturaDeRepetição/Desafio  52.py
@@ -1,4 +1,10 @@
 n = int(input('Digite um núemro: '))
-v = int(input('por qual número você quer dividir: '))
-primo = n / v
-print(primo)
+primo = True
+for c in range(2,n):
+    if n % c == 0:
+        primo = False
+        break
+if primo:
+    print('O  número {} é primo'.format(n))
+else:
+    print('O número {} não e primo'.format(n))
