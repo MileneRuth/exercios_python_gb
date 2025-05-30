265e396fd131a9f0ff3628a9337e279ff59bfb99
Milene Ruth <milene.ruth.ruth@outlook.com>
2025-05-30 16:56:03 -0300

    correção 52

--- EtruturaDeRepetição/Desafio  52.py
+++ EtruturaDeRepetição/Desafio  52.py
@@ -1,7 +1,14 @@
 n = int(input('Digite um número: '))
+tot = 0
 for c in range(1, n + 1):
     if n % c == 0:
         print('\033[1;33m', end= ' ')
+        tot += 1
     else:
         print('\033[1;31m',end=' ')
     print('{} '.format(c))
+print('\n \033[m0 número {} foi divisível {} vezes'.format(n,tot))
+if tot == 2:
+    print('E por isso que ele e PRIMO ')
+else:
+    print('Esse número NÂO e primo ')
