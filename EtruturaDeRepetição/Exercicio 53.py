168a60165bee09fb7babec2388f3fd939aae49c0
Milene Ruth <milene.ruth.ruth@outlook.com>
2025-06-09 11:47:47 -0300

    tentando

---
+++ EtruturaDeRepetição/Exercicio 53.py
@@ -0,0 +1,8 @@
+frase = str(input('Digite uma frase: ')).strip().replace(" ","").upper()
+print(frase)
+for c in range(frase):
+    if frase == frase:
+        print ('E uma palídromo')
+    else:
+        print('não e um políndromo')
+print('fim')
